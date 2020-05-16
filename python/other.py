8640fc8349d6cbc41b3b16ecd2c4d7fa6ce0c744
zhaohaoyu <747464004@qq.com>
2020-05-16 21:34:25 +0800

    push

--- python/other.py
+++ python/other.py
@@ -10,6 +10,8 @@ def timestamp_to_time(timestamp):
     return time.strftime("%Y-%m-%d %H:%M:%S", time_local)
 
 
+
+
 # 序列化时间
 def myconverter(o):
     if isinstance(o, datetime.datetime):
@@ -18,3 +20,4 @@ def myconverter(o):
 
 with open('fff.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(a, indent=2, ensure_ascii=False, default=myconverter) + '\n') 
+
